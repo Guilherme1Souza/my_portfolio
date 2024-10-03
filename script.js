cde50e85057ff25f5174626c06ae21b22e82d60b
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2024-10-03 18:02:35 -0300

    feat: adicionando botão CV e hiper links para redes sociais

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggle = document.getElementById("toggle");
 const body = document.getElementById('body');
 toggle.addEventListener('change', () => {
-  body.classList.toggle('dark');f
+  body.classList.toggle('dark');
 
 })
